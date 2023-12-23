6029ac363671dac25500913c7cbc935dbd95abfe
Sergio Ugarte <sergiouga85@gmail.com>
2023-12-23 16:18:42 -0300

    modificaciones

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 export const PORT= 8080
 export const MONGODB_CNX_STR= 'mongodb+srv://sergiouga85:0710.Serg@cluster0.mi2kbo9.mongodb.net/ecommerce'
-
 export const SESSION_SECRET = 'SecretCoder'
 
-export const githubAppId= '736824'
-export const githubClientId= ' Iv1.2b292e836ce0fde5'
-export const githubClientSecret='b7b7593b47e60a0fb5464bce88af1ef24338fff5'
+export const githubAppId= '738418'
+export const githubClientId= 'Iv1.49d5bfc3bc48b979'
+export const githubClientSecret='125860d96a0907172fe3a0dedb32f698e17cf245'
 export const githubCallbackUrl= 'http://localhost:8080/githubcallback'
